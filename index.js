c814814333859bd4ef77ff863767038bd5c2c519
Mahesh <jagzmz@gmail.com>
2020-02-14 18:31:14 +0530

    changes

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var STATUS_CODES;
 (function (STATUS_CODES) {
-    var AUTH;
+    let AUTH;
     (function (AUTH) {
         AUTH[AUTH["MODEL_NOT_FOUND"] = 1000] = "MODEL_NOT_FOUND";
         AUTH["MODEL_NOT_FOUND_DETAILS"] = " Model not found.";
@@ -44,32 +33,32 @@ var STATUS_CODES;
     })(AUTH = STATUS_CODES.AUTH || (STATUS_CODES.AUTH = {}));
     STATUS_CODES.CUSTOM_CODES = {};
     function getStatusText(statusCode) {
-        var numTotext = Object.values(STATUS_CODES).map(function (i) {
+        let numTotext = Object.values(STATUS_CODES).map((i) => {
             return i[statusCode];
         });
-        return numTotext.filter(function (x) { return x; })[0];
+        return numTotext.filter(x => x)[0];
     }
     STATUS_CODES.getStatusText = getStatusText;
     ;
     function getStatusNumber(statusCode) {
-        var textToNum = Object.values(STATUS_CODES).map(function (i) {
+        let textToNum = Object.values(STATUS_CODES).map((i) => {
             return i[statusCode];
         });
-        return textToNum.filter(function (i) { return i; })[0];
+        return textToNum.filter(i => i)[0];
     }
     STATUS_CODES.getStatusNumber = getStatusNumber;
     ;
     function getCustomStatusCode(statusCode) {
-        var numTotext = Object.values(STATUS_CODES.CUSTOM_CODES)
-            .map(function (context) {
-            return Object.keys(context).filter(function (key) { return context[key] === statusCode; })[0];
+        let numTotext = Object.values(STATUS_CODES.CUSTOM_CODES)
+            .map((context) => {
+            return Object.keys(context).filter((key) => context[key] === statusCode)[0];
         });
         return numTotext[0];
     }
     STATUS_CODES.getCustomStatusCode = getCustomStatusCode;
     function getCustomStatusNumber(statusCode) {
-        var numTotext = Object.values(STATUS_CODES.CUSTOM_CODES)
-            .map(function (context) {
+        let numTotext = Object.values(STATUS_CODES.CUSTOM_CODES)
+            .map((context) => {
             if (context.hasOwnProperty(statusCode)) {
                 return context[statusCode];
             }
@@ -78,7 +67,7 @@ var STATUS_CODES;
     }
     STATUS_CODES.getCustomStatusNumber = getCustomStatusNumber;
     function addStatusCode(type, keyVal) {
-        STATUS_CODES.CUSTOM_CODES[type] = __assign(__assign({}, STATUS_CODES.CUSTOM_CODES[type]), keyVal);
+        STATUS_CODES.CUSTOM_CODES[type] = Object.assign(Object.assign({}, STATUS_CODES.CUSTOM_CODES[type]), keyVal);
     }
     STATUS_CODES.addStatusCode = addStatusCode;
 })(STATUS_CODES = exports.STATUS_CODES || (exports.STATUS_CODES = {}));
